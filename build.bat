6200e53c2b221ebcde03dda2bcca7d1f6ebfb24a
Petr Mironychev <artifeks92@gmail.com>
2020-04-01 23:35:52 +0300

    Update appveyor.yml and build.bat

--- build.bat
+++ build.bat
@@ -1,3 +1,6 @@
 @echo off
 
-%QT_PATH%\bin\qmake.exe %APPVEYOR_BUILD_FOLDER%\qt-creator\qtcreator.pro
+REM %QT_PATH%\bin\qmake.exe %APPVEYOR_BUILD_FOLDER%\qt-creator\qtcreator.pro
+
+cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_PREFIX_PATH=QT_PATH;LLVM_INSTALL_DIR %APPVEYOR_BUILD_FOLDER%\qt-creator
+cmake --build .
