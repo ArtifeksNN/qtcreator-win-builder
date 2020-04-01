7d56b35a51fdb9ae0d70a7d2365a585f10f8bf9d
Petr Mironychev <artifeks92@gmail.com>
2020-04-01 23:46:11 +0300

    Update build.bat

--- build.bat
+++ build.bat
@@ -2,7 +2,7 @@
 
 REM %QT_PATH%\bin\qmake.exe %APPVEYOR_BUILD_FOLDER%\qt-creator\qtcreator.pro
 
-set PATH=%PATH%;C:\Qt\;C:\Qt\5.14.1\
+set PATH=%PATH%;C:\Qt\;C:\Qt\5.14.1\;C:\Qt\5.14.1\msvc2017_64\bin
 
 cmake -D CMAKE_BUILD_TYPE=Release -D CMAKE_PREFIX_PATH=QT_MSVC_PATH;LLVM_INSTALL_DIR %APPVEYOR_BUILD_FOLDER%\qt-creator
 cmake --build .
